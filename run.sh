e94b5ecc10808a53941e563782224736a33d396d
Ivan Panchenko <arigatory@gmail.com>
2024-05-10 19:04:30 +0300

    docker rebuild

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-export APP_MODULE=${APP_MODULE-app.main:app}
-export HOST=${HOST:-0.0.0.0}
-export PORT=${PORT:-8000}
-
 # exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 "$APP_MODULE"
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 "app.main:app"
