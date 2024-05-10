d60c9f31b91a8063a27f1a5433b2ca1b4bc23396
Ivan Panchenko <arigatory@gmail.com>
2024-05-10 18:45:52 +0300

    update

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import auth, role, user
 
 api_router = APIRouter()
+
 api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(role.router, tags=["role"])
 api_router.include_router(user.router, tags=["user"])
